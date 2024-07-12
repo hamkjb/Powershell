68a7c02fa2b2695afb8677c9f8a1d9e16e4cdd15
Kieran <108186641+hamkjb@users.noreply.github.com>
2024-07-12 10:55:11 +0100

    Update wingetupgrade.ps1

--- wingetupgrade.ps1
+++ wingetupgrade.ps1
@@ -1,8 +1,9 @@
 # Requires admin elevation
+# Requires admin elevation
 function Upgrade-WinGetPackages {
     try {
-        # Check for available upgrades and upgrade all packages
-        winget upgrade --all --source=winget --accept-package-agreements --accept-source-agreements
+        # Check for available upgrades and upgrade all packages, including those with unknown versions
+        winget upgrade --all --source=winget --accept-package-agreements --accept-source-agreements --include-unknown
     } catch {
         Write-Host "Failed to upgrade packages."
         Write-Host $_
