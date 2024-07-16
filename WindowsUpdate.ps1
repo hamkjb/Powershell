c2ed8b96ec476524ae4b027ae99615ff95d95e5b
Kieran <108186641+hamkjb@users.noreply.github.com>
2024-07-16 09:10:53 +0100

    Update WindowsUpdate.ps1

--- WindowsUpdate.ps1
+++ WindowsUpdate.ps1
@@ -1,5 +1,13 @@
 # Requires admin elevation
+
+# Install the PSWindowsUpdate module
 Install-Module -Name PSWindowsUpdate -Force
-Set-ExecutionPolicy RemoteSigned
+
+# Set the execution policy to RemoteSigned
+Set-ExecutionPolicy RemoteSigned -Force
+
+# Install all available updates and reboot automatically if needed
 Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+
+# Retrieve the Windows Update log
 Get-WindowsUpdateLog
